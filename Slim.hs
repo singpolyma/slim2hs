ebfee4bc09477673dedb0439a6b6d91e015e1c65
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-02-06 17:07:32 -0500

    Quoted attributes

--- Slim.hs
+++ Slim.hs
@@ -2,7 +2,9 @@
 module Slim where
 
 import Data.Char
+import Data.Maybe
 import Control.Applicative
+import Control.Monad
 import Text.Parsec hiding (optional, many, (<|>))
 import Text.Parsec.Indent
 import Control.Monad.Trans.Reader
@@ -10,16 +12,23 @@ import Control.Monad.Trans.Reader
 data SlimOutput = NoOutput | EscapedOutput | UnescapedOutput
 	deriving (Show)
 
-data Slim = Tag String [Slim] | Code SlimOutput String [Slim] | Text String | Comment Bool String
+data Slim = Tag String [SlimAttr] [Slim] | Code SlimOutput String [Slim] | Text String | Comment Bool String
+	deriving (Show)
+
+data SlimAttr = Attr String String
 	deriving (Show)
 
 data SlimConfig = SlimConfig {
-		shortcuts :: [Char]
+		shortcuts :: [Char],
+		attributeWrap :: [(Char,Char)]
 	}
 
-run sourceName p source = runIndent sourceName (runParserT (many1 p <* eof) () sourceName source)
+run sourceName p source = runIndent sourceName (runParserT (some p <* eof) () sourceName source)
 
-parser config =
+-- This forces unindents to be sane
+parser config = checkIndent >> parser' config
+
+parser' config =
 	textBlock Text "|" <|>
 	textBlock comment "/" <|>
 	withBlock ($) (code <* lineSepSpace True) (parser config) <|>
@@ -30,8 +39,8 @@ comment ('!':txt) = Comment True (dropWhile isSpace txt)
 comment txt = Comment False txt
 
 -- does folding
-textBlock cons sigil = (string sigil *> inlineSpaces *> pure (cons . concat)) <*/> textBlockLine
-textBlockLine = (++) <$> many1 (noneOf "\n") <*> (maybe "" (const $ " ") . snd <$> lineSepSpace True)
+textBlock cons sigil = withPos $ (string sigil *> inlineSpaces *> pure (cons . concat)) <*/> textBlockLine
+textBlockLine = (++) <$> some (noneOf "\n") <*> (maybe "" (const $ " ") . snd <$> lineSepSpace True)
 
 lineSepSpace reqNewline = inlineSpaces *> ((,) <$> nl <*> optional inlineSpace) <* inlineSpaces
 	where
@@ -42,11 +51,26 @@ inlineSpaces = skipMany (satisfy isInlineSpace) <?> "inline white space"
 inlineSpace = satisfy isInlineSpace <?> "inline white space character"
 isInlineSpace c = c /= '\n' && isSpace c
 
-tag (SlimConfig {shortcuts = s}) = (\x y -> Tag $ maybe y (:y) x) <$> optional (oneOf s) <*> many1 alphaNum
+tag config@(SlimConfig {shortcuts = s}) =
+	(\x y c -> Tag (maybe y (:y) x) c) <$> optional (oneOf s) <*> some alphaNum <*> attr config
+
+attr (SlimConfig {attributeWrap = wrap}) = (inlineSpaces *>) $ withPos $ foldr (<|>) (many $ attr' False) $
+	map (\(o,c) -> pure id <-/> string [o] <-/> inlineSpaces <*/> attr' True <-/> string [c]) wrap
+
+attr' allowAlone = do
+	key <- some alphaNum
+	val <- if allowAlone then optional valParser else fmap Just valParser
+	inlineSpaces
+	return $ Attr key (fromMaybe key val)
+	where
+	valParser = inlineSpaces *> string "=" *> inlineSpaces *>
+		(quotedString "\"" <|> quotedString "'")
+
+quotedString q = string q *> many (noneOf q) <* string q
 
 code = (\sigil txt -> Code (slimOutputSigil sigil) txt) <$>
 	(string "-" <|> try (string "==") <|> string "=") <*>
-	(inlineSpaces *> many1 (noneOf "\n"))
+	(inlineSpaces *> some (noneOf "\n"))
 
 slimOutputSigil "-" = NoOutput
 slimOutputSigil "=" = EscapedOutput
