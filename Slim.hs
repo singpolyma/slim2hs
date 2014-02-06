9c6cc24c6d07b4c89100afcd3d3bbe5747647918
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-02-06 16:21:51 -0500

    Custom shortcut characters

--- Slim.hs
+++ Slim.hs
@@ -5,6 +5,7 @@ import Data.Char
 import Control.Applicative
 import Text.Parsec hiding (optional, many, (<|>))
 import Text.Parsec.Indent
+import Control.Monad.Trans.Reader
 
 data SlimOutput = NoOutput | EscapedOutput | UnescapedOutput
 	deriving (Show)
@@ -12,13 +13,17 @@ data SlimOutput = NoOutput | EscapedOutput | UnescapedOutput
 data Slim = Tag String [Slim] | Code SlimOutput String [Slim] | Text String | Comment Bool String
 	deriving (Show)
 
+data SlimConfig = SlimConfig {
+		shortcuts :: [Char]
+	}
+
 run sourceName p source = runIndent sourceName (runParserT (many1 p <* eof) () sourceName source)
 
-parser =
+parser config =
 	textBlock Text "|" <|>
 	textBlock comment "/" <|>
-	withBlock ($) (code <* lineSepSpace True) parser <|>
-	withBlock ($) (tag <* lineSepSpace True) parser <|>
+	withBlock ($) (code <* lineSepSpace True) (parser config) <|>
+	withBlock ($) (tag config <* lineSepSpace True) (parser config) <|>
 	(lineSepSpace True *> pure (Comment False "")) -- HACK
 
 comment ('!':txt) = Comment True (dropWhile isSpace txt)
@@ -37,7 +42,7 @@ inlineSpaces = skipMany (satisfy isInlineSpace) <?> "inline white space"
 inlineSpace = satisfy isInlineSpace <?> "inline white space character"
 isInlineSpace c = c /= '\n' && isSpace c
 
-tag = (\x y -> Tag $ maybe y (:y) x) <$> optional (oneOf "#.") <*> many1 alphaNum
+tag (SlimConfig {shortcuts = s}) = (\x y -> Tag $ maybe y (:y) x) <$> optional (oneOf s) <*> many1 alphaNum
 
 code = (\sigil txt -> Code (slimOutputSigil sigil) txt) <$>
 	(string "-" <|> try (string "==") <|> string "=") <*>
